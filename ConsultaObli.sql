084fdab99e05b7eb8da9c02d07afa0316ad78034
rpresno <rjpresno@gmail.com>
2024-06-14 14:37:03 -0300

    3 SQL falta agregar filas

---
+++ ConsultaObli.sql
@@ -0,0 +1,3 @@
+/*4) Listar los datos de los pasajeros que hayan viajado a todos los destinos y que hayan pagado
+en efectivo. Considerar los vuelos únicamente cuyo piloto tenga más de 3000 horas de
+vuelo. Ordenar el resultado alfabéticamente por nombre del pasajero*/
